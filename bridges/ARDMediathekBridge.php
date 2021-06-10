37c4fd1a3a59d188bb8eb58486e1cf8edd2607c1
岳东辰 <38829129+yue-dongchen@users.noreply.github.com>
2021-06-11 07:29:14 +1000

    Update ARDMediathekBridge.php

--- bridges/ARDMediathekBridge.php
+++ bridges/ARDMediathekBridge.php
@@ -32,8 +32,8 @@ class ARDMediathekBridge extends BridgeAbstract {
     foreach($html->find('a.Root-sc-1ytw7qu-0') as $video) {
       $item = array();
       $item['uri'] = $video->href;
-	    echo($video->find('h3.H3-sc-1h18a06-4')->innertext);
-	    echo($video->find('div.Line-epbftj-1')->plaintext);
+	    echo($video->find('h3.H3-sc-1h18a06-4'));
+	    echo($video->find('div.Line-epbftj-1'));
       $item['title'] = $video->find('h3.H3-sc-1h18a06-4')->innertext;
       $item['timestamp'] = strtotime($video->find('div.Line-epbftj-1')->plaintext);
       // $item['enclosures']
